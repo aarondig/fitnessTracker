b987fc52ddbb06400313ef0750b4f1bc89266f4f
Aaron Diggdon <aarondiggdon@gmail.com>
2020-11-07 17:53:49 -0800

    Connected routes and added starter files

---
+++ Develop/models/Workout.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+
+const WorkoutSchema = new.Schem({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      type: { type: String },
+      name: { type: String },
+      duration: { type: Number },
+      weight: { type: Number },
+      reps: { type: Number },
+      sets: { type: Number },
+      distance: { type: Number },
+    },
+  ],
+});
+const Workout = mongoose.model("workout", WorkoutSchema);
+
+module.exports = Workout;
